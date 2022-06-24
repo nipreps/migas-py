de0ddd819aabd705b3643b759dbe844f9612141c
mathiasg <mathiasg@stanford.edu>
2022-06-24 13:22:24 -0400

    RF: Synchronize error messages, style

--- etelemetry/operations.py
+++ etelemetry/operations.py
@@ -19,31 +19,38 @@ class OperationTemplate(typing.TypedDict):
 
 def telemetry_check(func: typing.Callable) -> typing.Callable:
     """Decorator function to ensure telemetry collection is enabled."""
+
     @wraps(func)
     def can_send(*args, **kwargs):
         if not os.getenv("ENABLE_ET", "0").lower() in ("1", "true", "y", "yes"):
             # do not communicate with server
             return {
                 "success": False,
-                "message": "eTelemetry is not enabled."
+                "errors": [
+                    {
+                        "message": "eTelemetry is not enabled - set `ENABLE_ET` environment variable."
+                    }
+                ],
             }
         # otherwise, ensure config is set up
         setup()
         return func(*args, **kwargs)
+
     return can_send
 
 
 fromDateRange: OperationTemplate = {
-    'operation': 'query{from_date_range($)}',
-    'args': {
+    "operation": "query{from_date_range($)}",
+    "args": {
         # required
-        'project': '"{}"',
-        'start': '"{}"',
+        "project": '"{}"',
+        "start": '"{}"',
         # optional
-        'end': '"{}"',
-        'unique': '{}',
-    }
+        "end": '"{}"',
+        "unique": "{}",
+    },
 }
+
 
 @telemetry_check
 def from_date_range(
@@ -60,22 +67,23 @@ def from_date_range(
 
 
 addProject: OperationTemplate = {
-    'operation': 'mutation{add_project(p:{$})}',
-    'args': {
+    "operation": "mutation{add_project(p:{$})}",
+    "args": {
         # required
-        'project': '"{}"',
-        'project_version': '"{}"',
-        'language': '"{}"',
-        'language_version': '"{}"',
+        "project": '"{}"',
+        "project_version": '"{}"',
+        "language": '"{}"',
+        "language_version": '"{}"',
         # optional
-        'status': '{}',
-        'user': '"{}"',
-        'session': '"{}"',
-        'container': '{}',
-        'platform': '"{}"',
-        'arguments': '"{}"',
+        "status": "{}",
+        "user": '"{}"',
+        "session": '"{}"',
+        "container": "{}",
+        "platform": '"{}"',
+        "arguments": '"{}"',
     },
 }
+
 
 @telemetry_check
 def add_project(
@@ -89,17 +97,6 @@ def add_project(
     platform: str = None,
     arguments: list = None,
 ) -> dict:
-    # required:
-    # owner, repo, version
-    # language and language_version can be "assumed"
-    #
-    # extras:
-    # - arguments (not yet implemented)
-    # - container (unknown, docker, apptainer)
-    # - platform (linux, darwin, etc)
-    # - status (pending, success, fail)
-    # - userID (uuid - we can generate this)
-    # - sessionID (uuid - app will provide if they want)
     params = _introspec(add_project, locals())
     query = _formulate_query(params, addProject)
     status, response = request(Config.endpoint, query)
@@ -112,15 +109,14 @@ def _introspec(func: typing.Callable, func_locals: dict) -> dict:
 
     sig = inspect.signature(func)
     return {
-        param: func_locals[param] for param, val in sig.parameters.items()
+        param: func_locals[param]
+        for param, val in sig.parameters.items()
         if func_locals[param] != val.default
     }
 
 
 def _formulate_query(params: dict, template: OperationTemplate) -> str:
     """Construct the graphql query."""
-    qparams = {x: template['args'][x].format(params[x]) for x in template['args'] if x in params}
-    query = template['operation'].replace(
-        "$", ",".join([f'{x}:{y}' for x, y in qparams.items()])
-    )
+    qparams = {x: template["args"][x].format(params[x]) for x in template["args"] if x in params}
+    query = template["operation"].replace("$", ",".join([f"{x}:{y}" for x, y in qparams.items()]))
     return query
