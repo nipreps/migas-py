821857e40902bea5fd11b21695cb734c6b7bca5f
mathiasg <mathiasg@stanford.edu>
2022-06-27 20:03:55 -0400

    FIX: Rework `setup()` to allow overriding options

--- etelemetry/config.py
+++ etelemetry/config.py
@@ -15,9 +15,40 @@ File = typing.Union[str, Path]
 
 @dataclass
 class Config:
+    """
+    Class to store client-side configuration, facilitating communication with the server.
+
+    The class stores the following components:
+    - `endpoint`:
+    The URL of the etelemetry server
+    - `user_id`:
+    A string representation of a UUID (RFC 4122) assigned to the user.
+    - `session_id`:
+    A string representation of a UUID assigned to the lifespan of the etelemetry invocation.
+    """
     endpoint: str = None
-    user_id: uuid.UUID = None
+    user_id: str = None
+    session_id: str = None
     _is_setup = False
+
+    def _setup(self, endpoint: str = None, user_id: str = None, session_id: str = None) -> None:
+        if self._is_setup:
+            return
+        if endpoint is not None or self.endpoint is None:
+            self.endpoint = endpoint or DEFAULT_ENDPOINT
+        if user_id is not None or Config.user_id is None:
+            try:
+                uuid.UUID(user_id)
+                Config.user_id = user_id
+            except Exception:
+                Config.user_id = gen_uuid()
+        if session_id is not None or Config.session_id is None:
+            try:
+                uuid.UUID(session_id)
+                Config.session_id = session_id
+            except Exception:
+                Config.user_id = gen_uuid()
+        self._is_setup = True
 
 
 def load(filename: File = CONFIG_FILENAME) -> bool:
@@ -42,23 +73,35 @@ def save(filename: File = CONFIG_FILENAME) -> str:
     return str(filename)
 
 
-def setup(et_endpoint: str = None, user_id: uuid.UUID = None, filename: File = CONFIG_FILENAME):
-    """Configure the client, and save configuration to an output file."""
+def setup(
+    *,
+    endpoint: str = None,
+    user_id: uuid.UUID = None,
+    session_id: uuid.UUID = None,
+    save_config: bool = True,
+    filename: File = CONFIG_FILENAME,
+) -> None:
+    """
+    Configure the client, and save configuration to an output file.
+
+    This method is invoked before each API call, but can also be called by
+    application developers for finer-grain control.
+    """
     if Config._is_setup:
         return
     if Path(filename).exists():
-        return load(filename)
-    Config.endpoint = et_endpoint or DEFAULT_ENDPOINT
-    Config.user_id = user_id or gen_user_uuid()
-    Config._is_setup = True
-    save(filename)
+        load(filename)
+    # if any parameters have been set, override the current attribute
+    Config._setup(endpoint, user_id, session_id)
+    if save_config:
+        save(filename)
 
 
-def gen_user_uuid(uuid_factory: str = "safe") -> uuid.UUID:
+def gen_uuid(uuid_factory: str = "safe") -> str:
     """
-    Generate a user ID in UUID format.
+    Generate a RFC 4122 UUID.
 
-    Depending on what `uuid_factory` is provided, the user ID will be generated differently:
+    Depending on what `uuid_factory` is provided, the UUID will be generated differently:
     - `safe`: This is multiprocessing safe, and uses system information.
     - `random`: This is random, and may run into problems if setup is called across multiple
     processes.
@@ -70,12 +113,12 @@ def gen_user_uuid(uuid_factory: str = "safe") -> uuid.UUID:
     # TODO: 3.10 - Replace with match/case
     if uuid_factory == "safe":
         return _safe_uuid_factory()
-    if uuid_factory == "random":
-        return uuid.uuid4()
+    elif uuid_factory == "random":
+        return str(uuid.uuid4())
     raise NotImplementedError
 
 
-def _safe_uuid_factory() -> uuid.UUID:
+def _safe_uuid_factory() -> str:
     import getpass
     import socket
 
