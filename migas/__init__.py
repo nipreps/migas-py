fe00af6c46e0775c37a31171da50c33594ca7649
mathiasg <mathiasg@stanford.edu>
2022-07-26 15:43:08 -0400

    ENH: Add helper method to quickly view config

--- migas/__init__.py
+++ migas/__init__.py
@@ -2,12 +2,13 @@ from . import _version
 
 __version__ = _version.get_versions()['version']
 
-from .config import setup
+from .config import print_config, setup
 from .operations import add_project, get_usage
 
 __all__ = (
     "__version__",
     "add_project",
     "get_usage",
+    "print_config",
     "setup",
 )
