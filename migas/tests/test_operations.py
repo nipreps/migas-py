3b60ed948a1a9bfffeda3f932aab85a4e83d4355
mathiasg <mathiasg@stanford.edu>
2022-12-06 12:01:03 -0500

    TST: Clean up datetime logic

--- migas/tests/test_operations.py
+++ migas/tests/test_operations.py
@@ -1,29 +1,18 @@
+from datetime import datetime as dt, timezone as tz, timedelta
+
 import pytest
 
 from migas import __version__, setup
 from migas.operations import add_project, get_usage
+from .utils import do_server_tests
+
+# skip all tests in module if server is not available
+pytestmark = pytest.mark.skipif(not do_server_tests, reason="Local server not found")
 
 test_project = 'nipreps/migas-py'
-
-
-def _local_server_up() -> bool:
-    """Checks if the server is locally available."""
-    import requests
-
-    try:
-        res = requests.get('http://localhost:8000/', timeout=0.5)
-        assert res.headers.get('x-backend-server') == 'migas'
-    except Exception:
-        return False
-    return True
-
-pytestmark = pytest.mark.skipif(not _local_server_up(), reason="Local server not found")
-
-
-def future() -> str:
-    from datetime import datetime, timedelta
-
-    return (datetime.utcnow() + timedelta(days=2)).strftime('%Y-%m-%d')
+today = dt.now(tz.utc)
+future = (today + timedelta(days=2)).strftime('%Y-%m-%d')
+today = today.strftime('%Y-%m-%d')
 
 
 @pytest.fixture(scope='module', autouse=True)
@@ -52,15 +41,15 @@ def test_add_project():
 
 
 def test_get_usage():
-    res = get_usage(test_project)
+    res = get_usage(test_project, start=today)
     assert res['success'] is True
     all_usage = res['hits']
     assert res['hits'] > 0
 
-    res = get_usage(test_project, unique=True)
+    res = get_usage(test_project, start=today, unique=True)
     assert res['success'] is True
     assert all_usage >= res['hits'] > 0
 
-    res = get_usage(test_project, start=future())
+    res = get_usage(test_project, start=future)
     assert res['success'] is False
     assert res['hits'] == 0
