8d4985a48e73c8d7cf62b09b9435e11951f98158
mathiasg <mathiasg@stanford.edu>
2022-12-06 12:00:26 -0500

    TST: Move helper function to `test.utils`

---
+++ migas/tests/utils.py
@@ -0,0 +1,12 @@
+def _check_server_available() -> bool:
+    """Checks if the server is locally available."""
+    import requests
+
+    try:
+        res = requests.get('http://localhost:8000/', timeout=0.5)
+        assert res.headers.get('x-backend-server') == 'migas'
+    except Exception:
+        return False
+    return True
+
+do_server_tests = _check_server_available()
