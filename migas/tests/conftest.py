2241c12a2518ae3a05eb74eae7c961459bd4de9f
mathiasg <mathiasg@stanford.edu>
2026-04-02 10:56:42 -0400

    rf(tst): unify conftest, simplify test endpoint, fixture to mock requests

--- migas/tests/conftest.py
+++ migas/tests/conftest.py
@@ -1,16 +1,33 @@
 import pytest
-
+from collections import namedtuple
+from unittest.mock import MagicMock
 import migas
+from migas.tracker import _active_trackers
 
-TEST_ROOT = "http://localhost:8080/"
-TEST_ENDPOINT = TEST_ROOT
+TEST_ROOT = 'http://localhost:8080/'
+Mocks = namedtuple('Mocks', ['add_breadcrumb', 'request'])
 
+
+@pytest.fixture(scope='session')
+def endpoint() -> str:
+    """Assume tests are run with a local server - revisit if this changes"""
+    return TEST_ROOT
 
 
 @pytest.fixture(scope='module')
-def setup_migas():
+def setup_migas(endpoint):
     """Ensure migas is configured to communicate with the staging app."""
-    migas.setup(endpoint=TEST_ENDPOINT)
-
+    migas.setup(endpoint=endpoint)
     assert migas.config.Config._is_setup
     return migas.config.Config._is_setup
+
+
+@pytest.fixture
+def mock_requests(monkeypatch):
+    mock_add = MagicMock()
+    mock_req = MagicMock()
+    monkeypatch.setattr('migas.api.add_breadcrumb', mock_add)
+    monkeypatch.setattr('migas.request._request', mock_req)
+    _active_trackers.clear()
+    yield Mocks(mock_add, mock_req)
+    _active_trackers.clear()
